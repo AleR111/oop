9030cff8c1e052bdc1780b0e1113e7b5df26c485
Ершов Алексей Александрович <alexseier12345@gmail.com>
2022-08-01 17:30:59 +0300

    create constructor

---
+++ constructor/createConstructor.ts
@@ -0,0 +1,21 @@
+// Необходимо создать функцию-конструктор, которая создает пользователя с заданным именем (имя и фамилия) и возрастом.
+// А также, необходимо определить функции, sayName (возвращает полное имя) и has18 (true, если есть 18)
+
+function User(name, surname, age) {
+  this.name = name;
+  this.surname = surname;
+  this.age = age;
+}
+
+User.prototype.sayName = function () {
+  return `${this.name} ${this.surname}`;
+};
+User.prototype.has18 = function () {
+  return this.age >= 18;
+};
+
+const user = new User("Andrey", "Kobets", 32);
+
+console.log(user.has18()); // true
+
+console.log(user.sayName()); // 'Andrey Kobets'
